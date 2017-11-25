c0c8fa77e8a7f531aaa1c7ba0f9b3df269330391
simon <simon.jordan@mac.com>
2017-11-25 17:06:23 +0000

    updated to include a check for spaces (replace with nothing) and special characters

---
+++ main/src/beginner_sushi.py
@@ -0,0 +1,5 @@
+#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
+
+name = raw_input("What is your name?")
+print("Hello "+name)
+print("The Code, it's calling to you. Just let it in.")
